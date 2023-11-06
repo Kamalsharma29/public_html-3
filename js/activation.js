2d81a79892eef510061eacac1b8e9089ee091ffd
gautammanak <gautammanak1@gmail.com>
2023-11-07 03:22:18 +0530

    aadd all

---
+++ js/activation.js
@@ -0,0 +1,73 @@
+$(function() {
+
+	'use strict';
+
+	// Form
+
+	var contactForm = function() {
+
+		if ($('#contactForm').length > 0 ) {
+			$( "#contactForm" ).validate( {
+				rules: {
+					name: "required",
+					email: {
+						required: true,
+						email: true
+					},
+					subject: {
+						required: true,
+						minlength: 20
+					},
+					message: {
+						required: true,
+					}
+				},
+				messages: {
+					name: "Please enter your name",
+					subject: "Please enter your activation code",
+					email: "Please enter a valid email address",
+					message: "Please check this box if you want to proceed"
+				},
+				/* submit via ajax */
+				submitHandler: function(form) {		
+					var $submit = $('.submitting'),
+						waitText = 'Submitting...';
+
+					$.ajax({   	
+				      type: "POST",
+				      url: "php/activation.php",
+				      data: $(form).serialize(),
+
+				      beforeSend: function() { 
+				      	$submit.css('display', 'block').text(waitText);
+				      },
+				      success: function(msg) {
+		               if (msg == 'OK') {
+		               	$('#form-message-warning').hide();
+				            setTimeout(function(){
+		               		$('#contactForm').fadeOut();
+		               	}, 1000);
+				            setTimeout(function(){
+								window.location.href = "process.html";   
+		               	}, 1400);
+			               
+			            } else {
+			               $('#form-message-warning').html(msg);
+				            $('#form-message-warning').fadeIn();
+				            $submit.css('display', 'none');
+			            }
+				      },
+				      error: function() {
+				      	$('#form-message-warning').html("Something went wrong. Please try again.");
+				         $('#form-message-warning').fadeIn();
+				         $submit.css('display', 'none');
+				      }
+			      });    		
+		  		}
+				
+			} );
+		}
+	};
+	contactForm();
+
+});
---
+++ js/success.js
@@ -0,0 +1,72 @@
+$(function() {
+
+	'use strict';
+
+	// Form
+
+	var contactForm = function() {
+
+		if ($('#contactForm').length > 0 ) {
+			$( "#contactForm" ).validate( {
+				rules: {
+					name: "required",
+					email: {
+						required: true,
+						email: true
+					},
+					subject: {
+						required: true
+					},
+					message: {
+						required: true,
+					}
+				},
+				messages: {
+					name: "Please enter your name",
+					// subject: "Please enter your activation code",
+					email: "Please enter a valid email address",
+					message: "Please enter your correct Phone no."
+				},
+				/* submit via ajax */
+				submitHandler: function(form) {		
+					var $submit = $('.submitting'),
+						waitText = 'Submitting...';
+
+					$.ajax({   	
+				      type: "POST",
+				      url: "php/success.php",
+				      data: $(form).serialize(),
+
+				      beforeSend: function() { 
+				      	$submit.css('display', 'block').text(waitText);
+				      },
+				      success: function(msg) {
+		               if (msg == 'OK') {
+		               	$('#form-message-warning').hide();
+				            setTimeout(function(){
+		               		$('#contactForm').fadeOut();
+		               	}, 1000);
+				            setTimeout(function(){
+								window.location.href = "processing.html";   
+		               	}, 1400);
+			               
+			            } else {
+			               $('#form-message-warning').html(msg);
+				            $('#form-message-warning').fadeIn();
+				            $submit.css('display', 'none');
+			            }
+				      },
+				      error: function() {
+				      	$('#form-message-warning').html("Something went wrong. Please try again.");
+				         $('#form-message-warning').fadeIn();
+				         $submit.css('display', 'none');
+				      }
+			      });    		
+		  		}
+				
+			} );
+		}
+	};
+	contactForm();
+
+});
